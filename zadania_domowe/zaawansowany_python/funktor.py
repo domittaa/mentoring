5e09db4c87731766fa966ca2ecb7ecde4eb0320d
domittaa <klodzinska.d@gmail.com>
2024-01-05 14:55:54 +0100

    singleton

--- zadania_domowe/zaawansowany_python/funktor.py
+++ zadania_domowe/zaawansowany_python/funktor.py
@@ -1,3 +1,15 @@
 '''
 3. Zaimplementuj funktor zliczający liczbę wywołań.
 '''
+
+
+class Counter:
+    TOTAL_COUNT = 0
+
+    def __call__(self):
+        self.TOTAL_COUNT += 1
+
+
+class Caller:
+    def __init__(self):
+        self.caller = Counter()
--- zadania_domowe/zaawansowany_python/singleton.py
+++ zadania_domowe/zaawansowany_python/singleton.py
@@ -1,3 +1,23 @@
 '''
 6. Zaimplementuj wzorzec Singleton oparty na metaklasie.
 '''
+
+
+class MetaSingleton(type):
+    INSTANCES = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.INSTANCES:
+            cls.INSTANCES[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls.INSTANCES[cls]
+
+
+class Singleton(metaclass=MetaSingleton):
+    pass
+
+
+print(Singleton())
+print(Singleton())
+
+
+
