373de10cba6eeae53751ce426bf0791f03768ee5
domittaa <klodzinska.d@gmail.com>
2024-01-08 12:56:29 +0100

    deskryptory

--- zadania_domowe/zaawansowany_python/data_deskryptor.py
+++ zadania_domowe/zaawansowany_python/data_deskryptor.py
@@ -1,4 +1,35 @@
 '''
 8. Zaimplementuj data deskryptor, który będzie realizować funkcję validatora,
-czyli będzie weryfikować, czy ustawiona wartość jest zgodna z regułamy podanymi podczas jego inicjalizacji.
+czyli będzie weryfikować, czy ustawiona wartość jest zgodna z regułami podanymi podczas jego inicjalizacji.
 '''
+
+
+class DataDescriptorValidator:
+    def __init__(self, x):
+        self.x = x
+        if self.x < 0:
+            raise Exception("Value cannot be lower than 0")
+        elif self.x > 100:
+            raise Exception("Value cannot be higher than 100")
+
+    def __get__(self, obj, type):
+        print("Getting the x value..")
+        return self.x
+
+    def __set__(self, obj, val):
+        if val < 0:
+            raise Exception("Value cannot be lower than 0")
+        elif val > 100:
+            raise Exception("Value cannot be higher than 100")
+        else:
+            print(f"Setting the value to {val}")
+            self.x = val
+
+
+class MyClass:
+    obj = DataDescriptorValidator(5)
+
+
+m = MyClass()
+print(m.obj)
+m.obj = 120
--- zadania_domowe/zaawansowany_python/non_data_deskryptor.py
+++ zadania_domowe/zaawansowany_python/non_data_deskryptor.py
@@ -3,3 +3,28 @@
 a następnie będzie zwracać listę linków, które znajdują się na tej stronie.
 Wykorzystaj biiblioteki requests i beautifulsoup lub ich alternatywy.
 '''
+import requests
+from bs4 import BeautifulSoup
+
+
+class NonDataDescriptorLinks:
+    def __init__(self, url):
+        self.url = url
+
+    def __get__(self, obj, objtype):
+        reqs = requests.get(self.url)
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+        urls = []
+        for link in soup.find_all('a'):
+            url = link.get('href')
+            if url and url != '#':
+                urls.append(url)
+        return urls
+
+
+class MyClass:
+    x = NonDataDescriptorLinks('https://sii.pl/')
+
+
+m = MyClass()
+print(m.x)
